433b9c1d964b3a7241d2ed4e56387faa9453d6b0
Emmanuel Villavizar <evillavizartrinidad@gmail.com>
2021-01-09 22:54:41 -0400

    Style Sign In

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,6 +1,49 @@
 import React, { useState, useRef } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { FormHelperText } from "@material-ui/core";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © Notes App Emmanuel "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function Login() {
   const emailRef = useRef("");
@@ -10,6 +53,8 @@ function Login() {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
+  const classes = useStyles();
+
   async function handleSubmit(e) {
     e.preventDefault();
 
@@ -18,36 +63,78 @@ function Login() {
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
       history.push("/");
-    } catch {
+    } catch (err) {
       setError("Failed to sign in");
+      console.error(err);
     }
 
     setLoading(false);
   }
 
   return (
-    <>
-      <h4>{error && error}</h4>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="email" required ref={emailRef} placeholder="Email" />
-        <input
-          type="password"
-          required
-          ref={passwordRef}
-          placeholder="Password"
-        />
-        <button disabled={loading} type="submit">
-          Log In
-        </button>
-      </form>
-      <div>
-        Need an account? <Link to="/sign-up">Sign Up</Link>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <FormHelperText>{error && error}</FormHelperText>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            inputRef={emailRef}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            inputRef={passwordRef}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={loading}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="/forgot-password" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/sign-up" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
       </div>
-      <div>
-        Forgot your password? <Link to="/forgot-password"> Recover it</Link>
-      </div>
-    </>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
