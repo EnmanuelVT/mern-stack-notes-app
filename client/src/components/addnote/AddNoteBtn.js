f6b9e54ceaec14c8bb7b4b767bda925e039479a2
Emmanuel Villavizar <evillavizartrinidad@gmail.com>
2021-01-10 18:12:41 -0400

    Style AddNoteButton

--- client/src/components/addnote/AddNoteBtn.js
+++ client/src/components/addnote/AddNoteBtn.js
@@ -1,24 +1,55 @@
-import { Button } from "@material-ui/core";
+import { CssBaseline, Fab } from "@material-ui/core";
 import React, { useState } from "react";
 import Modal from "../modal/Modal";
 import AddNoteForm from "./AddNoteForm";
+import AddIcon from "@material-ui/icons/Add";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    position: "relative",
+    minHeight: "90vh",
+  },
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
 
 function AddNoteBtn() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const fab = {
+    color: "secondary",
+    className: classes.fab,
+    icon: <AddIcon />,
+    label: "Add",
+  };
 
   function handleAddNote() {
     setIsOpen(true);
   }
 
   return (
-    <div>
-      <Button onClick={handleAddNote} className="add-btn">
-        +
-      </Button>
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <AddNoteForm onClose={() => setIsOpen(false)} />
-      </Modal>
-    </div>
+    <>
+      <CssBaseline />
+      <div className={classes.root}>
+        <Fab
+          color={fab.color}
+          className={fab.className}
+          onClick={handleAddNote}
+        >
+          {fab.icon}
+        </Fab>
+        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          <AddNoteForm onClose={() => setIsOpen(false)} />
+        </Modal>
+      </div>
+    </>
   );
 }
 
